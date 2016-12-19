877c2febf31509d3ae21cee21fd59397d7a5a7a7
Ainor Syahrizal <macintorch@gmail.com>
2016-12-19 13:47:58 +0800

    seekbar function

--- app/src/main/java/ainor/com/my/eggtimer/MainActivity.java
+++ app/src/main/java/ainor/com/my/eggtimer/MainActivity.java
@@ -2,6 +2,8 @@ package ainor.com.my.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+
+        final TextView timerTextView = (TextView) findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(300);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int minutes = (int) i / 60;
+
+                int seconds = i - minutes * 60;
+
+                timerTextView.setText( Integer.toString(minutes) + ":" + Integer.toString(seconds));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
